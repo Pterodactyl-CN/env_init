e4569c8847b0b26e2854a30e29e06202629dd703
gzzchh <xjdzch@126.com>
2018-06-09 19:14:47 +0800

    更加干净了

--- Install/cron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-## Ask for the location of Oneinstack
-read -p "Please input the location of Oneinstack" one_install
-read -p "Please input the domain of Pterodactyl installation" pter_install
-
-#cat ${one_install}/options.conf | grep wwwroot_dir=/data/wwwroot 
-one_www_root_path=$( cat ${one_install}/options.conf | grep wwwroot_dir=/data/wwwroot | cut 12  )
-${{one_www_root_path}%=*}
